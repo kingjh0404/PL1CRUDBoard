3478f20391f24c05d38629b6650245c1217acddc
king0404 <xhakxh147@handong.ac.kr>
2022-12-03 13:21:39 +0900

    first commit

--- src/main/java/com/spring/board/BoardDAO.java
+++ src/main/java/com/spring/board/BoardDAO.java
@@ -1,2 +1,86 @@
-package com.spring.board;public class BoardDAO {
+package com.spring.board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+@Repository
+public class BoardDAO {
+//
+//    Connection conn = null;
+//    PreparedStatement stmt = null;
+//    ResultSet rs = null;
+@Autowired
+JdbcTemplate jdbcTemplate;
+
+//public void setTemplate(jdbcTemplate template){
+//
+//}
+
+    private final String BOARD_INSERT = "insert into BOARD2 (title,writer,content,category,topic) values (?,?,?,?,?)";
+    private final String BOARD_UPDATE = "update BOARD2 set title=?,writer=?, content=?, category, topic=? where seq=?";
+    private final String BOARD_DELETE = "delete from BOARD2  where seq=?";
+    private final String BOARD_GET = "select * from BOARD2  where seq=?";
+    private final String BOARD_LIST = "select * from BOARD2 order by seq desc";
+
+    public int insertBoard(BoardVO vo){
+        String sql = "insert into BOARD2 (title,writer,content,category,topic) values(" + "'" + vo.getTitle() + "'," + "'" + vo.getWriter() + "'," +"'"+vo.getContent()+"',"+"'"+vo.getCategory()+"',"+"'"+vo.getTopic()+"')";
+        return jdbcTemplate.update(sql);
+    }
+
+    // 글 삭제
+    public int deleteBoard(int seq) {
+        String sql = "delete from BOARD2 where seq = "+seq;
+        return jdbcTemplate.update(sql);
+    }
+    public int updateBoard(BoardVO vo) {
+       String sql = "update BOARD2 set title= '"+ vo.getTitle() +"',"
+               +"writer='"+vo.getWriter()+"',"
+               +"content='"+vo.getContent()+"',"
+               +"category='"+vo.getCategory()+"',"
+               +"topic='"+vo.getTopic()+"' where seq="+ vo.getSeq();
+       return jdbcTemplate.update(sql);
+    }
+
+
+    /*
+    * public int updateBoard(BoardVO vo) {
+        String sql="update BOARD set title='"+vo.getTitle()+"',"
+                +"writer='"+vo.getWriter()+"',"
+                +"content='"+vo.getContent()+"',"
+                +"category='"+vo.getCategory()+"' where seq="+vo.getSeq();
+        return jdbcTemplate.update(sql);
+    }
+    * */
+
+    public BoardVO getBoard(int seq) {
+        String sql = "select * from BOARD2 where seq="+ seq;
+        return jdbcTemplate.queryForObject(sql,new BoardRowMapper());
+    }
+
+    public List<BoardVO> getBoardList(){
+        String sql= "select * from BOARD2 order by regdate desc";
+        return jdbcTemplate.query(sql,new BoardRowMapper());
+    }
+    class BoardRowMapper implements RowMapper<BoardVO> {
+        @Override
+        public BoardVO mapRow(ResultSet rs, int rowNum)throws SQLException{
+            BoardVO vo = new BoardVO();
+            vo.setSeq(rs.getInt("seq"));
+            vo.setTitle(rs.getString("title"));
+            vo.setWriter(rs.getString("writer"));
+            vo.setContent(rs.getString("content"));
+            vo.setCategory(rs.getString("category"));
+            vo.setRegdate(rs.getDate("regdate"));
+            vo.setTopic(rs.getString("topic"));
+
+            return vo;
+
+        }
+    }
 }
--- src/main/webapp/WEB-INF/views/addpostform.jsp
+++ src/main/webapp/WEB-INF/views/addpostform.jsp
@@ -1,16 +1,28 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: kangjeonghui
-  Date: 2022/12/02
-  Time: 9:50 AM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+  <meta charset="UTF-8">
+  <title> 새글쓰기 </title>
+</head>
+<body>
+
+<h1>새글쓰기</h1>
+<form action="addok" method="post" >
+  <table id="edit">
+    <tr><td>제목:</td><td><input type="text" name="title"/></td></tr>
+    <tr><td>글쓴이:</td><td><input type="text" name="writer"/></td></tr>
+    <tr><td>내용:</td><td><textarea cols="50" rows="5" name="content"></textarea></td></tr>
+    <tr><td>카테고리:</td><td><input type="text" name="category"/></td></tr>
+    <tr><td>주제:</td><td><input type="text" name="topic"/></td></tr>
+
+    <%--<tr><td>Content:</td><td><textarea cols="50" rows="5" name="content"></textarea></td></tr>--%>
+<%--    <tr><td><a href="posts.jsp">View All Records</a></td><td align="right"><input type="submit" value="Add Post"/></td></tr>--%>
+  </table>
+<%--  <button type="button" onclick="location.href='list'">목록보기</button>--%>
+  <button type="submit">등록하기</button>
+</form>
+
+</body>
 </html>
